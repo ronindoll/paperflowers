37daf5f547712691890673ccd2238dc5bee9db2a
ronindoll <akamatsuheart@gmail.com>
2023-10-21 19:18:42 +0800

    skull emoji

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,23 +3,24 @@ if status is-interactive
 end
 
 alias please='sudo'
-
+# nala
 alias please-install='sudo nala install'
 alias please-remove='sudo nala remove'
 alias please-update='sudo nala update'
 alias please-upgrade='sudo nala upgrade'
 
-alias rosefetch='neofetch'
-alias rose='neofetch'
-
-abbr -a -- ni 'please-install'
-abbr -a -- nr 'please-remove'
-abbr -a -- nu 'please-update'
-abbr -a -- nunu 'please-upgrade'
 abbr -a -- pls please
+# nala
+abbr -a -- ns 'nala search'
 abbr -a -- ni please-install
 abbr -a -- nr please-remove
 abbr -a -- nu please-update
 abbr -a -- nunu please-upgrade
-abbr -a -- pls please
+
+# gay neofetch
+alias rosefetch='neofetch'
+alias rose='neofetch'
+
+# quality of life
 abbr -a -- mkdir 'mkdir -p'
+abbr -a -- tr trash-put
